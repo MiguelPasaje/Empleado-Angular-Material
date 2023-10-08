b63b55ca6922ab91ab66fca6390a9028d47741e8
MiguelPasaje <cartel-dy@hotmail.com>
2023-10-07 23:21:21 -0500

    angular-materia -> tabla con paginacion y navbar

---
+++ src/app/components/shared/angular-material/angular-material.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTableModule} from '@angular/material/table';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatSlideToggleModule,
+    CommonModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule
+  ],
+  exports:[
+    MatSlideToggleModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule
+  ]
+})
+export class AngularMaterialModule { }
