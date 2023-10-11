5206c9589fe53fd98675710d8450b42e103b92f8
MiguelPasaje <cartel-dy@hotmail.com>
2023-10-10 22:25:13 -0500

    form con angular material

--- src/app/components/add-edit-empleado/add-edit-empleado.component.ts
+++ src/app/components/add-edit-empleado/add-edit-empleado.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { MAT_RADIO_DEFAULT_OPTIONS } from '@angular/material/radio';
 
 @Component({
   selector: 'app-add-edit-empleado',
   templateUrl: './add-edit-empleado.component.html',
-  styleUrls: ['./add-edit-empleado.component.css']
+  styleUrls: ['./add-edit-empleado.component.css'],
+  providers: [{
+    provide: MAT_RADIO_DEFAULT_OPTIONS,
+    useValue: { color: 'primary' },
+}]
 })
 export class AddEditEmpleadoComponent {
+
+  estadosCiviles:any[] = ['sotero','Casado','Divorciado']
 
 }
